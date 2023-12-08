eda0983a9ffc0f03a52c95ccb84bc783e1273a04
patilpriyanka2325 <patilpriyanka2325@gmail.com>
2023-12-08 19:16:42 +0530

    new options has enetered

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("***** Welcome To Student Database Management System *****");
+        Console.WriteLine("Please select your choice\n 1.Add Student\n 2.View Student\n" +
+            " 3.View all students\n 4.Delete student\n 5.Exit");
+
+        var choice = Console.ReadLine();
     }
 }
